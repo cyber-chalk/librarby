98d109c080defdea86bbc8877b7e150eec85e54c
ItsDreadful <lukadvali2007@gmail.com>
2024-08-01 14:27:14 +1000

    Update lib.c for inverted index
    made the changes to work with the header file. msg me if there are any issues

--- lib.c
+++ lib.c
@@ -1,193 +1,231 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// merge sort by every way to sort and have that in the heap, so when the user
-// requests it, we will have it ready. When a addition/deletion is made,
-// we will update it in every list in the heap and the csv as well.
-// Concerning searching, we will use a inverted index which is best for
-// keywords.
+#include "library.h"
 
 // Popularity,Title,Author,Year,Return,Available
-typedef struct Book {
-  struct Book *next;
-  int popularity;
-  char title[100];
-  char author[100];
-  int year;
-  int returnD;
-  int available; // either 1 or 0, cant use bool
-} book;
-
-typedef enum sorts {
-  SORT_BY_POPULARITY,
-  SORT_BY_TITLE,
-  SORT_BY_AUTHOR,
-  SORT_BY_YEAR,
-  SORT_BY_RETURN_DATE,
-  SORT_BY_AVAILABILITY
-} SortBy;
 
 int compareBooks(const book *a, const book *b, SortBy criteria) {
-  switch (criteria) {
-  case SORT_BY_POPULARITY:
-    return a->popularity - b->popularity;
-  case SORT_BY_TITLE:
-    return strcmp(a->title, b->title);
-  case SORT_BY_AUTHOR:
-    return strcmp(a->author, b->author);
-  case SORT_BY_YEAR:
-    return a->year - b->year;
-  case SORT_BY_RETURN_DATE:
-    return a->returnD - b->returnD;
-  case SORT_BY_AVAILABILITY:
-    return a->available - b->available;
-  default:
-    return 0;
-  }
+    switch (criteria) {
+        case SORT_BY_POPULARITY:
+            return a->popularity - b->popularity;
+        case SORT_BY_TITLE:
+            return strcmp(a->title, b->title);
+        case SORT_BY_AUTHOR:
+            return strcmp(a->author, b->author);
+        case SORT_BY_YEAR:
+            return a->year - b->year;
+        case SORT_BY_RETURN_DATE:
+            return a->returnD - b->returnD;
+        case SORT_BY_AVAILABILITY:
+            return a->available - b->available;
+        default:
+            return 0;
+    }
 }
 
 book *readCSV(const char *filename) {
-  FILE *file = fopen(filename, "r");
-  if (file == NULL) {
-    printf("file not opening");
-    return NULL;
-  }
-  char line[256];
-  book *current = NULL;
-  book *head;
-
-  while (fgets(line, sizeof(line), file)) {
-    book *newBook = malloc(sizeof(book));
-
-    // - %d: Read an integer (popularity, year, returnD, available)
-    // - %99[^,]: Read a string (up to 99 characters or until a comma) (title,
-    // author)
-    sscanf(line, "%d,%99[^,],%99[^,],%d,%d,%d", &newBook->popularity,
-           newBook->title, newBook->author, &newBook->year, &newBook->returnD,
-           &newBook->available);
-    newBook->next = NULL;
-
-    if (current == NULL) {
-      current = newBook;
-      head = newBook;
-    } else {
-      current->next = newBook;
-      current = newBook;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("file not opening");
+        return NULL;
     }
-  }
-  fclose(file);
-  return head;
+    char line[256];
+    book *current = NULL;
+    book *head = NULL;
+
+    while (fgets(line, sizeof(line), file)) {
+        book *newBook = malloc(sizeof(book));
+        sscanf(line, "%d,%99[^,],%99[^,],%d,%d,%d", &newBook->popularity,
+               newBook->title, newBook->author, &newBook->year, &newBook->returnD,
+               &newBook->available);
+        newBook->next = NULL;
+
+        if (current == NULL) {
+            current = newBook;
+            head = newBook;
+        } else {
+            current->next = newBook;
+            current = newBook;
+        }
+    }
+    fclose(file);
+    return head;
 }
-// Merge sort is often preferred for sorting a linked list.
-// The slow random-access performance of a linked list makes other algorithms
-// like Quick Sort perform poorly, and other like Heap Sort completely
-// impossible. - Geeks for Geeks
 
 int linkSize(book *head) {
-  int size = 0;
-  while (head->next != NULL) {
-    size++;
-    head = head->next;
-  }
-  return size;
+    int size = 0;
+    while (head != NULL) {
+        size++;
+        head = head->next;
+    }
+    return size;
 }
 
 void divide(book *head, book **start, book **end) {
-  *start = head;
-  book *current = head->next;
-  book *prev = head;
+    *start = head;
+    book *current = head->next;
+    book *prev = head;
 
-  while (current != NULL) {
-    current = current->next;
-    if (current != NULL) {
-      prev = prev->next;
-      current = current->next;
+    while (current != NULL) {
+        current = current->next;
+        if (current != NULL) {
+            prev = prev->next;
+            current = current->next;
+        }
     }
-  }
-  *end = prev->next;
-  prev->next = NULL;
+    *end = prev->next;
+    prev->next = NULL;
 }
 
 book *sortedMerge(book *a, book *b, SortBy criteria) {
-  book *head; // maybe remove
+    book *result = NULL;
 
-  if (a == NULL)
-    return (b);
-  else if (b == NULL)
-    return (a);
-  if (compareBooks(a, b, criteria) <= 0) {
-    head = a;
-    head->next = sortedMerge(a->next, b, criteria);
-  } else {
-    head = b;
-    head->next = sortedMerge(a, b->next, criteria);
-  }
+    if (a == NULL)
+        return b;
+    else if (b == NULL)
+        return a;
 
-  return head;
+    if (compareBooks(a, b, criteria) <= 0) {
+        result = a;
+        result->next = sortedMerge(a->next, b, criteria);
+    } else {
+        result = b;
+        result->next = sortedMerge(a, b->next, criteria);
+    }
+    return result;
 }
 
 book *mergeSort(book *head, SortBy criteria) {
-  // int size = linkSize(head);
-  book *a;
-  book *b;
+    if (head == NULL || head->next == NULL)
+        return head;
 
-  if ((head == NULL) || (head->next == NULL))
-    return head;
-  divide(head, &a, &b);
+    book *a;
+    book *b;
+    divide(head, &a, &b);
 
-  mergeSort(a, criteria);
-  mergeSort(b, criteria);
+    a = mergeSort(a, criteria);
+    b = mergeSort(b, criteria);
 
-  return head = sortedMerge(a, b, criteria);
+    return sortedMerge(a, b, criteria);
 }
 
 void printList(book *head) {
-  book *current = head;
-  while (current != NULL) {
-    printf("Popularity: %d\n", current->popularity);
-    printf("--------------------------\n");
-    current = current->next;
-  }
+    book *current = head;
+    while (current != NULL) {
+        printf("Title: %s\nAuthor: %s\nPopularity: %d\nYear: %d\nReturn Date: %d\nAvailable: %d\n\n", 
+               current->title, current->author, current->popularity, 
+               current->year, current->returnD, current->available);
+        current = current->next;
+    }
 }
 
 book *copyList(book *head) {
-  if (head == NULL)
+    if (head == NULL)
+        return NULL;
+
+    book *newBook = malloc(sizeof(book));
+    if (newBook == NULL) {
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+
+    newBook->popularity = head->popularity;
+    strcpy(newBook->title, head->title);
+    strcpy(newBook->author, head->author);
+    newBook->year = head->year;
+    newBook->returnD = head->returnD;
+    newBook->available = head->available;
+    newBook->next = copyList(head->next);
+
+    return newBook;
+}
+
+InvertedIndex *createInvertedIndex(book *head) {
+    InvertedIndex *index = NULL;
+
+    book *current = head;
+    while (current != NULL) {
+        // Create keywords (for simplicity, consider only title and author)
+        char *keywords[] = {current->title, current->author};
+
+        for (int i = 0; i < 2; i++) {
+            InvertedIndex *existingNode = index;
+            InvertedIndex *prevNode = NULL;
+
+            while (existingNode != NULL && strcmp(existingNode->keyword, keywords[i]) != 0) {
+                prevNode = existingNode;
+                existingNode = existingNode->next;
+            }
+
+            if (existingNode == NULL) {
+                InvertedIndex *newNode = malloc(sizeof(InvertedIndex));
+                strcpy(newNode->keyword, keywords[i]);
+                newNode->books = copyList(current);
+                newNode->next = NULL;
+
+                if (prevNode == NULL) {
+                    index = newNode;
+                } else {
+                    prevNode->next = newNode;
+                }
+            } else {
+                book *bookList = existingNode->books;
+                while (bookList->next != NULL) {
+                    bookList = bookList->next;
+                }
+                bookList->next = copyList(current);
+            }
+        }
+        current = current->next;
+    }
+    return index;
+}
+
+book *searchInvertedIndex(InvertedIndex *index, const char *keyword) {
+    while (index != NULL) {
+        if (strcmp(index->keyword, keyword) == 0) {
+            return index->books;
+        }
+        index = index->next;
+    }
     return NULL;
-
-  // Allocate memory for the new book node
-  book *newBook = malloc(sizeof(book));
-  if (newBook == NULL) {
-    printf("Memory allocation failed\n");
-    return NULL;
-  }
-
-  // Copy the data from the original node to the new node
-  newBook->popularity = head->popularity;
-  strcpy(newBook->title, head->title);
-  strcpy(newBook->author, head->author);
-  newBook->year = head->year;
-  newBook->returnD = head->returnD;
-  newBook->available = head->available;
-
-  // Recursively copy the rest of the list
-  newBook->next = copyList(head->next);
-
-  return newBook;
 }
 
 int main() {
-  book *popularityH = readCSV("./data.csv");
-  book *titleH = mergeSort(copyList(popularityH), SORT_BY_TITLE);
-  book *authorH = mergeSort(copyList(popularityH), SORT_BY_AUTHOR);
-  book *yearH = mergeSort(copyList(popularityH), SORT_BY_YEAR);
-  book *returnH = mergeSort(copyList(popularityH), SORT_BY_RETURN_DATE);
-  book *availabilityH = mergeSort(copyList(popularityH), SORT_BY_AVAILABILITY);
+    book *popularityH = readCSV("./data.csv");
+    if (popularityH == NULL) {
+        return 1;
+    }
 
-  //  on front-end disconnection make sure to free
-  //  Also! may need to move the code above this to main.c
-  printList(titleH);
-  // printList(readCSV("./data.csv"));
+    // Merge sort by every way to sort and have that in the heap, so when the user
+    // requests it, we will have it ready. When an addition/deletion is made,
+    // we will update it in every list in the heap and the csv as well.
+    // Concerning searching, we will use an inverted index which is best for
+    // keywords.
 
-  return 0;
+    book *titleH = mergeSort(copyList(popularityH), SORT_BY_TITLE);
+    book *authorH = mergeSort(copyList(popularityH), SORT_BY_AUTHOR);
+    book *yearH = mergeSort(copyList(popularityH), SORT_BY_YEAR);
+    book *returnH = mergeSort(copyList(popularityH), SORT_BY_RETURN_DATE);
+    book *availabilityH = mergeSort(copyList(popularityH), SORT_BY_AVAILABILITY);
+
+    // On front-end disconnection make sure to free
+    // Also! may need to move the code above this to main.c
+
+    // Create the inverted index
+    InvertedIndex *index = createInvertedIndex(popularityH);
+
+    char keyword[100];
+    printf("Enter a keyword to search: ");
+    scanf("%99s", keyword);
+
+    book *result = searchInvertedIndex(index, keyword);
+    if (result != NULL) {
+        printList(result);
+    } else {
+        printf("No results found for keyword: %s\n", keyword);
+    }
+
+    return 0;
 }
